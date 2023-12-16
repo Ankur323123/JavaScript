01f499edd9475b3842e0df3bb519f1fc5784b7d7
Ankur323123 <154007214+Ankur323123@users.noreply.github.com>
2023-12-16 16:48:28 +0000

    Done

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 135436
+let accountEmail= "ankur@gmail.com"
+var accountPassword = "qwqe@idngi"
+accountCity = "Mumbai"
+
+ // accountId = 2 // this is comment
+accountEmail ="Poddar@gmail.com"
+accountPassword = "dfsf@egmgm"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountCity])
